b82fa6a5ec09b1d20cd3e606f26335ed4e8912f4
Kudrin German <kudringermann@gmail.com>
2023-03-22 20:28:24 +0200

    add structure

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from rest_framework.response import Response 
+from rest_framework.views import APIView
+
+class WomenAPIView(APIView):
+    def get(self, request):
+        return Response({'get': 'get'})
+    
+    def post(self, request):
+        return Response({'post': 'post'})
+    
+    def put(self, request):
+        return Response({'put': 'put'})
